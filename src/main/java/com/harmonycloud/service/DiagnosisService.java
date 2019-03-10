8985b3c7a9314a7240b9e49d5fbc911b05aa2c69
dongqi <dongqi@harmonycloud.cn>
2019-03-10 12:26:15 +0800

    update check number

--- src/main/java/com/harmonycloud/service/DiagnosisService.java
+++ src/main/java/com/harmonycloud/service/DiagnosisService.java
@@ -30,25 +30,22 @@ public class DiagnosisService {
 
         // 根据id查询
         try {
-            boolean isNum = isNumeric(keyword);
-            if (isNum) {
-                Integer keywordNum = Integer.valueOf(keyword);
-                Diagnosis diagnose = diagnosisMonRepository.findByDiagnosisId(keywordNum);
-                diagnosesList.add(diagnose);
-            }
+            Integer keywordNum = Integer.valueOf(keyword);
+            Diagnosis diagnose = diagnosisMonRepository.findByDiagnosisId(keywordNum);
+            diagnosesList.add(diagnose);
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
         return Result.buildSuccess(diagnosesList);
     }
 
-    public boolean isNumeric(String str){
-        for (int i = str.length();--i>=0;){
-            if (!Character.isDigit(str.charAt(i))){
-                return false;
-            }
-        }
-        return true;
-    }
+//    public boolean isNumeric(String str){
+//        for (int i = str.length()-1;i>=0;i--){
+//            if (!Character.isDigit(str.charAt(i))){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
 }
