478d2f70b0eeb2af0a2b604b331b4718c90c7036
dongqi <dongqi@harmonycloud.cn>
2019-03-15 15:55:44 +0800

    update

--- src/main/java/com/harmonycloud/main/DiagnosisApplication.java
+++ src/main/java/com/harmonycloud/main/DiagnosisApplication.java
@@ -4,6 +4,8 @@ import org.apache.servicecomb.saga.omega.spring.EnableOmega;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -16,7 +18,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories("com.harmonycloud.oraRepository")
 @EntityScan("com.harmonycloud.entity")
 @EnableOmega
-public class DiagnosisApplication {
+public class DiagnosisApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DiagnosisApplication.class, args);
